37e6909480d9eeb7a5c76c0951fc6fddcacf5805
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-02-26 18:58:42 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [   "@nuxtjs/supabase", '@nuxtjs/tailwindcss', '@nuxt/content'  ]
+    modules: [   "@nuxtjs/supabase", '@nuxtjs/tailwindcss', '@nuxt/content'  ],
+    runtimeConfig: {
+        supabaseUrl: process.env.SUPABASE_URL ,
+        supabaseKey : process.env.SUPABASE_KEY
+    }
 
 })
