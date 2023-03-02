75ba2dc77a9831eeadd2f2c7d25e84532eafe7ab
JhinAhri12 <cyprien.rezki@hotmail.fr>
2023-03-02 19:12:54 +0100

    fix middleware

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -4,12 +4,21 @@ export default defineNuxtRouteMiddleware((to, _from) => {
       return navigateTo("/user/signup");
     }
     else{
-        const admin = async() => await $fetch(`/api/user/admin`, {
-            method: 'POST',
-            body: {event: 'IS_ADMIN', email: user.value?.email}
-        })
-        if (admin.isAdmin = true){
-            return navigateTo("/");
+        console.log(user.value?.email)
+        const asyncGetCall = async () => {
+        try {
+            const response = await $fetch(`/api/user/admin`, {
+                method: 'POST',
+                body: {event: 'IS_ADMIN', email: user.value?.email}});
+            if(!response){
+                return navigateTo("/");
+            }
+            // enter you logic when the fetch is successful
+           } catch(error) {
+        // enter your logic for when there is an error (ex. error toast)
+              console.log(error)
+             } 
         }
+        asyncGetCall()
     }
   });
